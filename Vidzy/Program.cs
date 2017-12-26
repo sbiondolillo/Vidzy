1c6ad81936c1ce507e5fc4652d89c69641a02dcd
Samuel Biondolillo <biwinning121@gmail.com>
2017-12-26 15:21:53 -0500

    change target framework to .NET

--- Vidzy/Program.cs
+++ Vidzy/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Vidzy
 {
@@ -6,7 +10,6 @@ namespace Vidzy
     {
         static void Main(string[] args)
         {
-
         }
     }
 }
